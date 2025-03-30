1118cd84629f119bf6aa6d26ba0826ce00e1f624
Bhuvan <bhuvanptr@gmail.com>
2025-03-30 21:46:20 +0530

    last

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,6 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: mode === "production" ? "/Your_Resume/" : "/", // Replace <REPO_NAME> with your GitHub repository name
   server: {
     host: "::",
     port: 8080,
